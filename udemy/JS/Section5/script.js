ec79ed9296ab71cdce025a310ca9285ed6ef6c1c
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2020-06-04 17:01:07 +0900

    Clousures

--- udemy/JS/Section5/script.js
+++ udemy/JS/Section5/script.js
@@ -88,14 +88,14 @@ function calculateAge(ele) {
 }
 
 // 콜백 함수()
-function isFullAge(el) {
-    return el >= 18;
+function isFullAge(ele) {
+    return ele >= 18;
 }
 
 // 콜백 함수(3)
-function maxHeartRate(el) {
-    if (el >= 18 && el <= 81) {
-        return Math.round(206.9 - 0.67 * el);
+function maxHeartRate(ele) {
+    if (ele >= 18 && ele <= 81) {
+        return Math.round(206.9 - 0.67 * ele);
     } else {
         return -1;
     }
@@ -134,20 +134,71 @@ teacherQuestion("John");
 designerQuestion("Jane");
 
 interviewQuestion("designer")("Mark");
-*/
 
 ///////////////////////////////////
 // 즉시 실행 함수 표현 (IIFE: Immediately Invoked Functions Expressions)
 function game() {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
+    var score = Math.random() * 10;
+    console.log(score >= 5);
 }
 game();
 
 // IIFE Function
 var result = (function () {
-  var score = Math.random() * 10;
-  return console.log(score >= 5);
+    var score = Math.random() * 10;
+    return console.log(score >= 5);
 })();
 
 result;
+
+*/
+
+///////////////////////////////////
+// 클로저
+function retirement(retirementAge) {
+  var a = " years left until retirement.";
+  return function (yearofBirth) {
+    var age = 2020 - yearofBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+// retirement(66)(1990);
+
+// 클로저 사용하기
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function (name) {
+      console.log(name + ", can you please explain what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function (name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+function interviewQuestion(job) {
+  return function (name) {
+    if (job === "designer") {
+      console.log(name + ", can you please explain what UX design is?");
+    } else if (job === "teacher") {
+      console.log("What subject do you teach, " + name + "?");
+    } else {
+      console.log("Hello " + name + ", what do you do?");
+    }
+  };
+}
+
+interviewQuestion("teacher")("John");
