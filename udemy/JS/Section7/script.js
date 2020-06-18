6e67b22b1e0c7b33b829fd8021668051e8017c09
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2020-06-18 12:55:33 +0900

    106. Strings(문자열)
    `(백틱)
     ${}

--- udemy/JS/Section7/script.js
+++ udemy/JS/Section7/script.js
@@ -70,23 +70,62 @@ for (var i = 0; i < 5; i++) {
 }
 
 console.log(i);
-*/
 
 /////////////////////////////////////////////
 // Lecture: Blocks and IIFEs
 
 // ES6
 {
-  const a = 1;
-  let b = 2;
-  var c = 3;
+    const a = 1;
+    let b = 2;
+    var c = 3;
 }
 // console.log(a + b);
 console.log(c);
 
 // ES5
 (function () {
-  var c = 3;
+    var c = 3;
 })();
 
 // console.log(c);
+
+*/
+
+/////////////////////////////////////////////
+// Lecture: Strings
+
+let firstName = "John";
+let lastName = "Smith";
+const yearOfBirth = 1990;
+
+function clacAge(year) {
+  return 2020 - year;
+}
+
+// ES5
+console.log(
+  "This is " +
+    firstName +
+    " " +
+    lastName +
+    ". He was born in " +
+    yearOfBirth +
+    ". Today, he is " +
+    clacAge(yearOfBirth) +
+    " years old."
+);
+
+// ES6
+console.log(
+  `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${clacAge(
+    yearOfBirth
+  )} years old. `
+);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith("J"));
+console.log(n.endsWith("th"));
+console.log(n.includes(" "));
+console.log(firstName.repeat(5));
+console.log(`${firstName} `.repeat(5));
