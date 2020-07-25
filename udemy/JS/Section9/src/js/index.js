461ae8ec1cccdbdb2213d2f7c3750719af88dd1e
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2020-07-25 12:47:36 +0900

    140. API calls
    npm install axios --save

--- udemy/JS/Section9/src/js/index.js
+++ udemy/JS/Section9/src/js/index.js
@@ -1,8 +1,16 @@
-// Global app controller
-import num from "./test";
+import axios from "axios"; // 에디터가 경로를 스스로 알아냄.
 
-const x = 23;
-
-console.log(
-  ` I imported ${num} from another module called test.js! Variable x is ${x}`
-);
+async function getResults(query) {
+  try {
+    // fetch는 오랜 된 브라우저에서는 인식되지 않을 수 있다.
+    // --> axios를 사용.
+    const res = await axios(
+      `https://forkify-api.herokuapp.com/api/search?q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults("pizza");
