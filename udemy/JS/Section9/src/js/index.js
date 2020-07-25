436969e490a378efd014233e18125d496cf886eb
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2020-07-25 13:15:59 +0900

    141. Search Models
    class modules.
    import / export
    new 키워드로 인스턴스 생성

--- udemy/JS/Section9/src/js/index.js
+++ udemy/JS/Section9/src/js/index.js
@@ -1,16 +1,5 @@
-import axios from "axios"; // 에디터가 경로를 스스로 알아냄.
+import Search from "./models/Search";
 
-async function getResults(query) {
-  try {
-    // fetch는 오랜 된 브라우저에서는 인식되지 않을 수 있다.
-    // --> axios를 사용.
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults("pizza");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
--- udemy/JS/Section9/src/js/models/Search.js
+++ udemy/JS/Section9/src/js/models/Search.js
@@ -1,3 +1,22 @@
-const res = await axios(
-  `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
-);
+import axios from "axios"; // 에디터가 경로를 스스로 알아냄.
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    try {
+      // fetch는 오랜 된 브라우저에서는 인식되지 않을 수 있다.
+      // --> axios를 사용.
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?q=${this.query}`
+      );
+
+      this.result = res.data.recipes;
+      //   console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
