cf34943c10ef293a608458fee87ae69993ae0049
CH4MD0M <started677@gmail.com>
2020-09-03 14:42:44 +0900

    Day 1
    32. Component 생성
    34. Component  재사용
    35. 동적 컨텐츠 사용
    36. props 사용
    37. children 사용

--- udemy/React/Section3/src/App.js
+++ udemy/React/Section3/src/App.js
@@ -9,8 +9,14 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <h1>Another Heading</h1>
         <p>This is really working!</p>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
       </div>
-      // <h1>Another Heading</h1> --> 하나의 루트 요소를 반환하기 때문에 루트 요소안에 들어있어야 함.
+
+      // <h1> Another Heading </h1> --> 하나의 루트 요소를 반환하기 때문에 루트 요소안에 들어있어야 함.
       // html처럼 보이지만 html이 아니라 jsx이다.
     );
     // return React.createElement("div", { className: "App" }, React.createElement("h1", "null", "Hi, I'm a React App!!"));
