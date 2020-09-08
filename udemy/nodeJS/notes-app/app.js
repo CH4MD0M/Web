d56eeb7e32418ef27ce0c1c13e05bed130c19647
CH4MD0M <started677@gmail.com>
2020-09-08 10:11:28 +0900

    10. Importing other files
    module.exports

--- udemy/nodeJS/notes-app/app.js
+++ udemy/nodeJS/notes-app/app.js
@@ -1,4 +1,21 @@
-const fs = require("fs");
-fs.writeFileSync("notes.txt", "My name is Ch4md0m.");
+// const fs = require("fs");
+// fs.writeFileSync("notes.txt", "My name is Ch4md0m.");
+// fs.appendFileSync("notes.txt", " (data to append)");
 
-fs.appendFileSync("notes.txt", " (data to append)");
+// //////////////////////////////
+// 10. Importing Own files
+// Variable
+const firstName = require("./utils");
+const add = require("./utils");
+// const name = "Chamdom";
+console.log(firstName);
+
+// Function
+const sum = add(1, 2);
+console.log(sum);
+
+//Challenge
+const getNotes = require("./notes");
+
+const msg = getNotes();
+console.log(msg);
